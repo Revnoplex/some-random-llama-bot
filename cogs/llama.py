fc070c4066c80fdae894ded9fd06dd75caec4cb6
Revnoplex <62947003+Revnoplex@users.noreply.github.com>
2025-03-15 15:29:27 +1100

    add gemma3

--- cogs/llama.py
+++ cogs/llama.py
@@ -485,6 +485,81 @@ class Ollama(config.RevnobotCog):
                 )
             )
 
+    # noinspection SpellCheckingInspection,PyTypeHints
+    @bridge.bridge_command(
+        name='ask-gemma3',
+        description="Ask the gemma 3 llm",
+        integration_types={discord.IntegrationType.guild_install, discord.IntegrationType.user_install}
+    )
+    @commands.cooldown(**config.default_cooldown_options)
+    async def ask_gemma3_cmd(
+            self, ctx: bridge.Context, *, prompt: BridgeOption(str, "Prompt to send to gemma3"),
+            image: BridgeOption(
+                discord.Attachment, "The image to show the model", required=False
+            ) = None
+    ):
+        """About the bot?"""
+        await ctx.defer()
+        images = []
+        if (ctx.message and len(ctx.message.attachments) > 0) or image:
+            image_bytes = await (
+                ctx.message.attachments[0] if ctx.message and len(ctx.message.attachments) > 0
+                else image
+            ).read()
+            images.append(image_bytes)
+        try:
+            await self.ollama_client.ps()
+        except (httpx.ConnectError, httpx.TimeoutException, ConnectionError):
+            app = await ctx.bot.application_info()
+            await ctx.respond(embed=utils.default_embed(
+                ctx, "Cannot Connect to Ollama Server.",
+                "Unable to connect to ollama server as it is probably not running. "
+                f"Please ask {app.owner.mention} to start the ollama server."
+            ))
+            return
+        except httpx.HTTPError as error:
+            await ctx.respond(embed=utils.default_embed(
+                ctx, "Error Connecting to Ollama Server.",
+                f"There was a problem trying to connect to the ollama server: {error}"
+            ))
+            return
+        try:
+            async with ctx.typing() if not isinstance(ctx, discord.ApplicationContext) else nullcontext():
+                message = {
+                    'role': 'user',
+                    'content': prompt,
+                    'images': images,
+                    'system': 'your response will be sent over discord, so please make sure your entire '
+                              'response is limited to 4096 characters'
+                }
+                if ctx.channel.id not in context_bank:
+                    context_bank[ctx.channel.id] = []
+                context_bank[ctx.channel.id].append(message)
+                response = await self.ollama_client.chat(
+                    model="gemma3:27b-it-fp16", messages=context_bank[ctx.channel.id]
+                )
+        except ollama.ResponseError as error:
+            await ctx.respond(embed=utils.default_embed(
+                ctx, "Error Genrating Response",
+                f"{error.error}"
+            ))
+            return
+        context_bank[ctx.channel.id].append({'role': 'assistant', 'content': response.message.content})
+        if len(response.message.content) <= 2000:
+            await ctx.respond(f"{response.message.content}")
+        elif len(response.message.content) <= 4096:
+            await ctx.respond(
+                embed=utils.default_embed(ctx, "Gemma 3 Response", f"{response.message.content}")
+            )
+        else:
+            await ctx.respond(
+                embed=utils.default_embed(
+                    ctx, "Response Too large",
+                    f"Gemma 3 sent a response longer that the maximum amount of characters allowed on discord "
+                    f"(4096). Please tell gemma3 to limit the response to 4096 characters."
+                )
+            )
+
 
 def setup(client):
     client.add_cog(Ollama(client))
